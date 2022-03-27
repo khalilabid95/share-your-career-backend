b62295d3efd190d4f52fabe3cdf2c1d05ccd1c1f
Khalil Abid <khalilabid98@gmail.com>
2022-03-27 19:46:06 +0200

    add technologie route

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
-
+const cors = require('cors')
 const  mongoose  = require('mongoose');
 require('dotenv').config();
 const app = express()
 app.use(express.json());
 const usersRoute = require('./routes/users')
-
+const TECH =[
+    {
+    _id: "1",
+    name: "React"
+},
+{
+    _id: "2",
+    name: "Node"
+},
+]
 app.get('/',(req,res)=>{
     res.json({message: "welecome"})
 })
-
+app.get('/technologies', (req,res) =>{
+    res.json({TECH})
+})
 app.use('/auth',usersRoute)
+app.use(cors({ credentials: true, origin: [process.env.WEB_APP_URL] }))
 
 
-PORT =process.env.PORT || 3000
+PORT =process.env.PORT || 5000
 mongoose.connect(process.env.Data_Base_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Successfully connected to MongoDB');
